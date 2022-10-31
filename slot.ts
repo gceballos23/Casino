bb8ccc60998e8289d7cad6990d98e90c49664a33
German Ceballos <germanceballos@tdf.edu.ar>
2022-10-31 20:10:53 -0300

    1 TRAGAMONEDA - FALTA IMPLEMENTACION

--- slot.ts
+++ slot.ts
@@ -1,15 +1,18 @@
 export class Slot{
     protected figuras : string[];
     protected UltimaJugada : string;
+    protected posicion :number
 
     constructor(pFiguras: string[]){
         this.figuras = pFiguras;
         this.UltimaJugada = this.figuras[0];
-
+        this.posicion = 0;
     }
 
     public girarSlots():void{
-       this.UltimaJugada =this.figuras[Math.floor(Math.random() * this.figuras.length)];
+       let posicion : number =  Math.floor(Math.random() * this.figuras.length);
+       this.UltimaJugada =this.figuras[posicion];
+       this.setPosicon(posicion);
     }
 
     public getFiguraMayor():string{
@@ -18,5 +21,13 @@ export class Slot{
 
     public getUltimaJugada():string{
         return this.UltimaJugada;
+    }
+
+    protected setPosicon(pIndice : number):void{
+        this.posicion = pIndice;
+    }
+    
+    public getposicion():number{
+        return this.posicion;
     }
 }
--- tragamonedas.ts
+++ tragamonedas.ts
@@ -8,9 +8,10 @@ export class Tragamoneda{
     protected historialJugadas : string[];
     protected dineroIngresado: number;
     protected pozoTotal: number;
-    protected jugadaPozoTotal : string;
+    protected jugadaPozoTotal : string[];
     protected apuestaMinima : number;
     protected apuesta:number;
+    protected premio : number;
 
     constructor(ptipoSlot:Slot, pCantidadSlot:number,pPozoTotal:number){
         this.tipoSlot = ptipoSlot;
@@ -21,9 +22,10 @@ export class Tragamoneda{
         this.historialJugadas=[];
         this.dineroIngresado=0;
         this.pozoTotal= pPozoTotal;
-        this.jugadaPozoTotal = this.tipoSlot.getFiguraMayor();
+        this.setJugadaPozoTotal();
         this.apuestaMinima = 1;
         this.apuesta = this.apuestaMinima;
+        this.premio= 0;
     
     }
 
@@ -41,12 +43,25 @@ export class Tragamoneda{
         return this.ultimaJugada;
     }
 
+    protected setUltimaJugada():void{ 
+        this.ultimaJugada= [];
+        for( let i:number = 0; i<this.cantidadSlot; i++){
+            this.slots[i].girarSlots();
+            this.ultimaJugada.push(this.slots[i].getUltimaJugada());
+        } 
+    }
+    
+
     public getDineroIngesado():number{
         return this.dineroIngresado;
     }
 
     public IngresarDinero(pCantidad):void{
         this.dineroIngresado = this.dineroIngresado + pCantidad;
+    }
+
+    protected restarDineroIngresado():void{
+        this.dineroIngresado = this.dineroIngresado - this.apuesta;
     }
 
     public getPozoTotal(): number{
@@ -70,7 +85,7 @@ export class Tragamoneda{
         }
     }
 
-    protected esApuestaGanadora():boolean{
+    protected probabilidadGanar():boolean{
         /* probalidad de 1 en 20 */
         let probabilidad : number[]=[1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1]
         if  (probabilidad[Math.floor(Math.random() * probabilidad.length)] ===0){
@@ -80,13 +95,94 @@ export class Tragamoneda{
         } 
     }
 
-    public TirarPalanca(){
-        this.ultimaJugada = [];
-        for( let i:number = 0; i<this.cantidadSlot; i++){
-            this.ultimaJugada.push(this.slots[i].getUltimaJugada());
-        }      
-        
+    public getHistorialJugada():string[]{
+        return  this.historialJugadas;
     }
 
+    protected setHistorialJugadas():void{
+        for( let i:number = 0; i<this.cantidadSlot; i++){
+            this.historialJugadas.push(this.slots[i].getUltimaJugada());
+        }  
+    }
+
+    protected getJugadaPozoTotal():string[]{
+        return this.jugadaPozoTotal
+    }
+
+    protected setJugadaPozoTotal():void{
+        for( let i:number = 0; i<this.cantidadSlot; i++){
+            this.jugadaPozoTotal.push(this.slots[i].getFiguraMayor());
+        }  
+    }
+
+    protected GanarPozo():boolean{
+        if (this.jugadaPozoTotal = this.ultimaJugada) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    protected SaberSiEsJugadaGanadora():boolean{
+        let ganadora : boolean = false;
+        for( let i:number = 0; i<this.cantidadSlot - 1; i++){
+            if (this.slots[i].getUltimaJugada() === this.slots[i +1].getUltimaJugada()){
+                ganadora =   true;
+            } else {
+                ganadora = false;
+                break;
+
+            }
+        } 
+        return ganadora;
+
+    }
+
+    protected setPremio(pPremio:number):void{
+        this.premio = pPremio;
+    }
+
+    public getPremio():number{
+        return this.premio;
+    }
+
+    protected resetPremioPozo():void{
+        this.pozoTotal = 100;
+    }
+
+
+    protected calcularPremio():number{
+        return this.apuesta *  (this.slots[0].getposicion() + 1) * 10;
+    }
+
+    public TirarPalanca(){
+        let control :number = 0;
+        let probabilidad :boolean = this.probabilidadGanar(); 
+        while (control ===  0 ) {       
+            this.setUltimaJugada;            
+            if ( this.SaberSiEsJugadaGanadora() === probabilidad ) {
+                control = 1;
+            }
+
+        }
+
+        this.setHistorialJugadas();
+
+        if (this.SaberSiEsJugadaGanadora()){
+            if (this.GanarPozo()){
+                this.setPremio(this.getPozoTotal())
+                this.IngresarDinero(this.getPremio);
+                this.resetPremioPozo;
+
+            }else{
+                this.setPremio(this.calcularPremio())
+                this.IngresarDinero(this.getPremio());
+                this.incrementarPozoTotal();
+            }
+        } else {
+            this.restarDineroIngresado();
+        }
+
+    }
 
 }
